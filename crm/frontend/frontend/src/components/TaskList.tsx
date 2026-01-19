0204f907216e7f99addc3b14315cc544af32d385
Escudo5 <sergiomargar03@gmail.com>
2026-01-19 16:57:21 +0100

    empiezo conexion fullstack

--- crm/frontend/frontend/src/components/TaskList.tsx
+++ crm/frontend/frontend/src/components/TaskList.tsx
@@ -1,3 +1,30 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+
+type Task = {
+    id: number;
+    title: string;
+    description: string;
+    project: string;
+};
+
 export default function TaskList() {
+
+
+
+    const [tasks, setTasks] = useState<Array<Task>>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchTasks = async () => {
+            setLoading(true);
+            setError(null);
+            try {
+                const response = await fetch('http://localhost:8000/api/tasks/', {
+                    headers: {
+                        
+
   return <div><p>Lista de tareas</p></div>;
 }
